a926360283888d0a2d5082d0431a0b0acdb67ba7
Hank Tom <hanktom@gmail.com>
2016-10-24 20:24:26 +0800

    onCreateViewHolder

--- app/src/main/java/com/tom/atm/TransactionAdapter.java
+++ app/src/main/java/com/tom/atm/TransactionAdapter.java
@@ -1,7 +1,10 @@
 package com.tom.atm;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -10,12 +13,30 @@ import java.util.ArrayList;
  * Created by Administrator on 2016/10/24.
  */
 
-public class TransactionAdapter {
+public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.ViewHolder>{
 
     private final ArrayList<Transaction> trans;
 
     public TransactionAdapter(ArrayList<Transaction> trans) {
         this.trans = trans;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        View view = LayoutInflater.from(context).inflate(R.layout.trans_row, null);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return trans.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
