3607344757143135842b595cb785a74b94ad7e8b
Hank Tom <hanktom@gmail.com>
2016-10-27 19:54:53 +0800

    Permission done

--- app/src/main/java/com/tom/atm/ContactActivity.java
+++ app/src/main/java/com/tom/atm/ContactActivity.java
@@ -1,11 +1,12 @@
 package com.tom.atm;
 
-import android.Manifest;
+import static android.Manifest.permission.*;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.Contacts;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,21 +14,33 @@ import android.util.Log;
 
 public class ContactActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CONTACTS = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
         int permission =
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
+                ActivityCompat.checkSelfPermission(this, READ_CONTACTS);
         if (permission != PackageManager.PERMISSION_GRANTED){
             //未
-            
+            ActivityCompat.requestPermissions(this,
+                    new String[]{READ_CONTACTS},
+                    REQUEST_CONTACTS );
         }else{
             //已
             readContacts();
         }
+    }
 
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode==REQUEST_CONTACTS){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                readContacts();
+            }
+        }
     }
 
     private void readContacts() {
