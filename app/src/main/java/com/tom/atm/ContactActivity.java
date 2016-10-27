8bbb4a806121f87898cc443ac65564dadae8ccf7
Hank Tom <hanktom@gmail.com>
2016-10-27 19:31:26 +0800

    Permission-0

--- app/src/main/java/com/tom/atm/ContactActivity.java
+++ app/src/main/java/com/tom/atm/ContactActivity.java
@@ -1,8 +1,12 @@
 package com.tom.atm;
 
+import android.Manifest;
 import android.content.ContentResolver;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.provider.ContactsContract.Contacts;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +17,20 @@ public class ContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        int permission =
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
+        if (permission != PackageManager.PERMISSION_GRANTED){
+            //未
+            
+        }else{
+            //已
+            readContacts();
+        }
+
+
+    }
+
+    private void readContacts() {
         ContentResolver cr = getContentResolver();
         Cursor c = cr.query(Contacts.CONTENT_URI, null, null, null, null);
         while(c.moveToNext()){
@@ -20,6 +38,5 @@ public class ContactActivity extends AppCompatActivity {
             int id = c.getInt(c.getColumnIndex(Contacts._ID));
             Log.d("CC", id+"/"+name);
         }
-
     }
 }
