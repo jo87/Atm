a8c720243d300e4dd82d8326b4321a01e42e79d6
Hank Tom <hanktom@gmail.com>
2016-10-31 19:36:47 +0800

    縮圖(未完成)

--- app/src/main/java/com/tom/atm/PictureActivity.java
+++ app/src/main/java/com/tom/atm/PictureActivity.java
@@ -11,24 +11,39 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.GridView;
+import android.widget.SimpleCursorAdapter;
 
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 
 public class PictureActivity extends AppCompatActivity {
 
     private static final int REQUEST_STORAGE = 110;
+    private GridView grid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
+        grid = (GridView) findViewById(R.id.grid);
         int permission = ActivityCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE);
         if (permission== PackageManager.PERMISSION_GRANTED){
-            readPictures();
+            readThumbnails();
+//            readPictures();
         }else{
             ActivityCompat.requestPermissions(this, new String[]{READ_EXTERNAL_STORAGE},
             REQUEST_STORAGE);
         }
+    }
+
+    private void readThumbnails() {
+        Cursor c = getContentResolver().query(Thumbnails.EXTERNAL_CONTENT_URI,
+                null, null, null, null);
+        String[] from = {Thumbnails.DATA, Thumbnails.IMAGE_ID};
+        int[] to = {R.id.thumb_image, R.id.thumb_text};
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                R.layout.thumb_item, c, from , to, 0);
+        grid.setAdapter(adapter);
     }
 
     @Override
@@ -36,7 +51,8 @@ public class PictureActivity extends AppCompatActivity {
                                            @NonNull int[] grantResults) {
         if (requestCode == REQUEST_STORAGE &&
                 grantResults[0]==PackageManager.PERMISSION_GRANTED){
-            readPictures();
+//            readPictures();
+            readThumbnails();
         }
     }
 
@@ -53,9 +69,9 @@ public class PictureActivity extends AppCompatActivity {
         Cursor c2 = cr.query(Thumbnails.EXTERNAL_CONTENT_URI,null, null, null, null, null);
         while (c2.moveToNext()){
             int id = c2.getInt(c2.getColumnIndex(Thumbnails._ID));
-            String name = c2.getString(c2.getColumnIndex(Thumbnails.IMAGE_ID));
+            String imageId = c2.getString(c2.getColumnIndex(Thumbnails.IMAGE_ID));
             String data = c2.getString(c2.getColumnIndex(Thumbnails.DATA));
-            Log.d("CC", id+","+name+","+data);
+            Log.d("CC", id+","+imageId+","+data);
         }
 
     }
